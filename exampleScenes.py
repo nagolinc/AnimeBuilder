6b3d366fa2477fec980e850ec6b48f2583f8a4a6
Logan zoellner <nagolinc@gmail.com>
2023-03-18 18:19:13 -0400

    add exampleScenes.py to improve scene generation with chatGPT

---
+++ exampleScenes.py
@@ -0,0 +1,174 @@
+exampleScenesPrompt = "Jack fights the dragon"
+
+exampleScenesResult = {
+    1: """
+scene 1 summary:
+Jack confronts the dragon, exchanging blows and narrowly avoiding being scorched by the dragon's fiery breath.
+""", 2: """
+scene 1 summary:
+Jack prepares for battle, sharpening his sword and donning his armor.
+
+scene 2 summary:
+Jack confronts the dragon, exchanging blows and narrowly avoiding being scorched by the dragon's fiery breath.
+""", 3: """
+scene 1 summary:
+Jack approaches the dragon's lair with his sword drawn.
+scene 2 summary:
+Jack engages in a fierce battle with the dragon, dodging flames and swiping at its scales.
+scene 3 summary:
+Jack lands a final blow, slaying the dragon and emerging victorious.
+""", 4: """
+scene 1 summary:
+Jack prepares for battle with the dragon, sharpening his sword and donning his armor.
+scene 2 summary:
+Jack engages in a fierce battle with the dragon, dodging flames and attacking with his sword.
+scene 3 summary:
+The dragon gains the upper hand, knocking Jack to the ground and breathing fire directly at him.
+scene 4 summary:
+Jack strikes a fatal blow to the dragon, finally defeating the beast and saving the kingdom.
+""", 5: """
+scene 1 summary:
+Jack prepares for battle by sharpening his sword and putting on his armor
+scene 2 summary:
+Jack confronts the dragon, who breathes fire at him
+scene 3 summary:
+Jack dodges the dragon's attacks and strikes back with his sword
+scene 4 summary:
+The dragon grabs Jack in its claws, but Jack manages to stab it in the eye
+scene 5 summary:
+The dragon falls to the ground and Jack emerges victorious, but injured from the battle.
+""", 6: """
+scene 1 summary:
+Jack finds the dragon's lair and prepares for battle.
+scene 2 summary:
+The dragon emerges from its lair, breathing fire and ready to fight.
+scene 3 summary:
+Jack charges at the dragon with his sword, but the dragon evades his attacks.
+scene 4 summary:
+The dragon grabs Jack with its claws, lifting him off the ground.
+scene 5 summary:
+Jack manages to break free by stabbing the dragon in the eye with his sword.
+scene 6 summary:
+The dragon falls to the ground, defeated, and Jack emerges victorious.
+""", 7: """
+scene 1 summary:
+Jack prepares for battle, sharpening his sword and donning his armor.
+scene 2 summary:
+Jack approaches the dragon's lair, cautiously scanning the surrounding area for any signs of danger.
+scene 3 summary:
+The dragon emerges from the lair, breathing flames and snarling fiercely.
+scene 4 summary:
+Jack charges at the dragon, dodging its fiery breath and striking it with his sword.
+scene 5 summary:
+The dragon retaliates, lashing out with its powerful tail and knocking Jack off his feet.
+scene 6 summary:
+Jack manages to wound the dragon, causing it to retreat back into its lair.
+scene 7 summary:
+Jack chases the dragon into the lair and jumps on top of it stabbing it repeatedly. The dragon falls to the ground, defeated, and Jack emerges victorious.
+""", 8: """
+scene 1 summary:
+Jack prepares for battle, sharpening his sword and donning his armor.
+scene 2 summary:
+Jack sets out to find the dragon, determined to defeat it.
+scene 3 summary:
+Jack encounters the dragon and engages in a fierce battle.
+scene 4 summary:
+The dragon breathes fire, but Jack manages to dodge and strike back.
+scene 5 summary:
+Jack's sword breaks, forcing him to improvise and use his surroundings to his advantage.
+scene 6 summary:
+The dragon's scales prove tough to penetrate, but Jack finally lands a critical blow.
+scene 7 summary:
+Jack manages to wound the dragon, causing it to retreat back into its lair.
+scene 8 summary:
+Jack chases the dragon into the lair and jumps on top of it stabbing it repeatedly. The dragon falls to the ground, defeated, and Jack emerges victorious.
+""", 9: """
+scene 1 summary:
+Jack prepares for battle, sharpening his sword and donning his armor.
+scene 2 summary:
+Jack approaches the dragon's lair, cautiously scanning his surroundings for any sign of danger.
+scene 3 summary:
+The dragon emerges from its lair, breathing fire and roaring fiercely.
+scene 4 summary:
+Jack charges towards the dragon with his sword drawn, dodging the flames as he goes.
+scene 5 summary:
+The dragon swings its tail, narrowly missing Jack and causing him to stumble.
+scene 6 summary:
+Jack strikes the dragon with a powerful blow, but the dragon retaliates with a fierce bite.
+scene 7 summary:
+Jack manages to dodge the dragon's next attack and strikes it again, causing it to roar in pain.
+scene 8 summary:
+The dragon becomes enraged and charges towards Jack, who narrowly avoids its attack and delivers the final blow.
+scene 9 summary:
+Jack stands victorious over the defeated dragon, breathing heavily and surveying the aftermath of the battle.
+""", 10: """
+scene 1 summary:
+Jack prepares for the battle with the dragon
+scene 2 summary:
+Jack and the dragon face off in a fierce battle
+scene 3 summary:
+Jack dodges a fireball and strikes the dragon with his sword
+scene 4 summary:
+The dragon retaliates with a powerful tail swipe, knocking Jack to the ground
+scene 5 summary:
+Jack regains his footing and charges back into battle
+scene 6 summary:
+The dragon attempts to fly away, but Jack grabs onto its tail and pulls it back down
+scene 7 summary:
+Jack climbs onto the dragon's back and delivers a devastating blow to its neck
+scene 8 summary:
+The dragon thrashes around wildly, trying to shake Jack off
+scene 9 summary:
+Jack clings on for dear life as the dragon crashes to the ground, defeated
+scene 10 summary:
+Jack triumphantly raises his sword in victory over the slain dragon
+""", 11: """
+scene 1 summary:
+Jack prepares to face the dragon.
+scene 2 summary:
+Jack approaches the dragon's lair.
+scene 3 summary:
+The dragon emerges from its lair and breathes fire.
+scene 4 summary:
+Jack dodges the dragon's fire breath and charges forward.
+scene 5 summary:
+Jack swings his sword at the dragon but misses.
+scene 6 summary:
+The dragon swipes at Jack with its claws, narrowly missing him.
+scene 7 summary:
+Jack notices a weak spot in the dragon's armor and aims for it.
+scene 8 summary:
+Jack strikes the weak spot and the dragon roars in pain.
+scene 9 summary:
+The dragon attempts to take off and fly away, but Jack grabs onto its tail.
+scene 10 summary:
+Jack climbs onto the dragon's back and drives his sword into its neck, killing it.
+scene 11 summary:
+Jack triumphantly raises his sword in victory over the slain dragon
+""", 12: """
+scene 1 summary:
+Jack approaches the dragon's lair with a sword in hand
+scene 2 summary:
+The dragon emerges from the cave, breathing fire
+scene 3 summary:
+Jack charges at the dragon, dodging flames
+scene 4 summary:
+The dragon swipes at Jack with its claws
+scene 5 summary:
+Jack lands a blow on the dragon's leg, causing it to stumble
+scene 6 summary:
+The dragon knocks Jack back with its tail
+scene 7 summary:
+Jack picks himself up and charges again
+scene 8 summary:
+The dragon grabs Jack in its jaws, but he manages to break free
+scene 9 summary:
+Jack climbs onto the dragon's back and stabs it in the neck
+scene 10 summary:
+The dragon roars in pain and tries to shake Jack off
+scene 11 summary:
+Jack holds on tight and delivers the final blow, slaying the dragon
+scene 12 summary:
+Jack emerges from the dragon's lair, victorious and covered in blood
+"""
+}
